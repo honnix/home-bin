7764e124135e4955767dabd4a633f82906e66150
ehonlia <hongxin.liang@ericsson.com>
2015-07-15 14:47:21 +0200

    typo

--- docker_add_port.sh
+++ docker_add_port.sh
@@ -2,7 +2,7 @@
 
 CONTAINER_IP=$1
 CONTAINER_PORT=$2
-HOST_IP=$3
+HOST_PORT=$3
 
 iptables -t nat -A POSTROUTING -s $CONTAINER_IP/32 -d $CONTAINER_IP/32 -p tcp -m tcp --dport $CONTAINER_PORT -j MASQUERADE
 iptables -t nat -A DOCKER ! -i docker0 -p tcp -m tcp --dport $HOST_PORT -j DNAT --to-destination $CONTAINER_IP:$CONTAINER_PORT
